fd264a73168c84222e3c915be38bac6bd2c61b90
Ifat BR <ifat.3d@gmail.com>
2021-02-25 18:55:36 +0200

    hamburger icon

--- js/cmps/nav-bar.cmp.js
+++ js/cmps/nav-bar.cmp.js
@@ -1,10 +1,10 @@
 export default{
     template:`
-    <nav>   
+    <nav class="nav-bar">   
         <router-link to="/">Home</router-link>
         <router-link to="/email">Email</router-link>
         <router-link to="/keep">Keep</router-link>
-        <button class="burger-nav-bar">M</button>
+        <button class="burger-nav-bar"></button>
     </nav>
     `
 }
